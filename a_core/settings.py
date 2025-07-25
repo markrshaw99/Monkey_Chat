e07b61c39ffd9ac38037dd3222b7d46c28529240
markrshaw99 <markshaw1999@gmail.com>
2025-07-25 11:15:27 +0100

    Fixed leave chatrrom

--- a_core/settings.py
+++ a_core/settings.py
@@ -191,7 +191,6 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
