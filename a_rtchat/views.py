2c21c4111e07b96dac813d09512844eb1ab94d79
markrshaw99 <markshaw1999@gmail.com>
2025-07-25 10:26:48 +0100

    Fixed leave chatrrom

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -129,3 +129,14 @@ def chatroom_delete_view(request, chatroom_name):
         return redirect('home')
     
     return render(request, 'a_rtchat/chatroom_delete.html', {'chat_group': chat_group})
+
+@login_required
+def chatroom_leave_view(request, chatroom_name):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+    if request.user not in chat_group.members.all():
+        raise Http404("You are not a member of this chat room.")
+    
+    if request.method == 'POST':
+        chat_group.members.remove(request.user)
+        messages.success(request, "You have left the chat room.")
+        return redirect('home')
