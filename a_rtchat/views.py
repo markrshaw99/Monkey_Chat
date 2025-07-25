135eb595852f9e26253deeb46b3de011cae19e4e
markrshaw99 <markshaw1999@gmail.com>
2025-07-25 10:37:42 +0100

    Fixed leave chatrrom

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -8,7 +8,7 @@ from .forms import *
 
 
 @login_required
-def chat_view(request, chatroom_name='public-chat'):
+def chat_view(request, chatroom_name='Public Chat'):
     chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
     chat_messages = chat_group.chat_messages.all()[:30]  # Get the last 30 messages
     form = ChatMessageCreateForm()
